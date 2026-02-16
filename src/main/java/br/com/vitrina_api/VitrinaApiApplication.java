a04f231360cfa6e7b4fe48c5c57c077568b6b1b3
PedroTrudes <pedro.jtrudes@gmail.com>
2026-02-16 17:31:56 -0300

    first commit

---
+++ src/main/java/br/com/vitrina_api/VitrinaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitrina_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitrinaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitrinaApiApplication.class, args);
+	}
+
+}
