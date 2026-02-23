ccf10147242ff2ee10a928617702202aa4f44d03
PedroTrudes <pedro.jtrudes@gmail.com>
2026-02-23 17:20:56 -0300

    feat: getAll users and update model for UUID

--- src/main/java/br/com/vitrina_api/controller/UserController.java
+++ src/main/java/br/com/vitrina_api/controller/UserController.java
@@ -3,8 +3,12 @@ package br.com.vitrina_api.controller;
 import br.com.vitrina_api.model.User;
 import br.com.vitrina_api.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -17,8 +21,13 @@ public class UserController {
         return userService.create(user);
     }
 
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        return ResponseEntity.ok(userService.findAll());
+    }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/br/com/vitrina_api/service/UserService.java
+++ src/main/java/br/com/vitrina_api/service/UserService.java
@@ -5,6 +5,9 @@ import br.com.vitrina_api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -19,7 +22,11 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public void delete(Long id){
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public void delete(UUID id){
         User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não localizado"));
         userRepository.delete(user);
     }
