9bf52db46d34a796a5264ed37fad785260be85f9
PedroTrudes <pedro.jtrudes@gmail.com>
2026-03-25 01:29:52 -0300

    feat: criando a entidade Invite no banco de dados

---
+++ src/main/java/br/com/vitrina_api/modules/invitation/model/Invite.java
@@ -0,0 +1,51 @@
+package br.com.vitrina_api.modules.invitation.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "invites")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Invite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "public_id", unique = true, nullable = false, updatable = false)
+    private UUID publicId;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(name = "store_id",nullable = false)
+    private UUID storeId;
+
+    @Column(nullable = false)
+    private UUID createdUserId;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiresAt;
+
+    @PrePersist
+    public void onCreate(){
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.expiresAt = now.plusDays(12);
+        this.active = true;
+        this.publicId = UUID.randomUUID();
+    }
+
+}
